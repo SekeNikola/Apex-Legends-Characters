c2511568e06dda17ad62c8facbf0bf19a1631f8e
Seke Nikola <sekenikola@gmail.com>
2019-04-12 10:23:14 +0200

    seting up a project

---
+++ src/components/characters/bloodhound/bloodhound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const bloodhound = () => {
+  return (
+    <>
+      <img src={require("../../../assets/img/bangalor.png")} alt="" />
+    </>
+  );
+};
+
+export default bloodhound;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const menu = () => {
+  return (
+    <div className="menu">
+      <ul>
+        <li>BloodHound</li>
+        <li>Gibraltar</li>
+        <li>Lifeline</li>
+        <li>Pathfinder</li>
+        <li>Octane</li>
+        <li>Wraith</li>
+        <li>Bangalor</li>
+        <li>Caustic</li>
+        <li>Mirage</li>
+      </ul>
+    </div>
+  );
+};
+
+export default menu;
